13c8946b28a8be24da4e40ee30f3382cd8b81d70
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-01-21 10:30:11 +0530

    Reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag_optimized(tag) for tag in post.tags.popular()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -35,6 +35,13 @@ def serialize_tag(tag):
     return {
         'title': tag.title,
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
+    }
+
+
+def serialize_tag_optimized(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.num_related_posts,
     }
 
 
@@ -56,7 +63,7 @@ def index(request):
             serialize_post_optimized(post) for post in most_fresh_posts
         ],
         'popular_tags': [
-            serialize_tag(tag) for tag in most_popular_tags
+            serialize_tag_optimized(tag) for tag in most_popular_tags
         ],
     }
     return render(request, 'index.html', context)
